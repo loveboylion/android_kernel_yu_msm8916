00b907e729da967e6d88b8030fcc65301b43ec7c
Scott Mertz <scott@cyngn.com>
2015-01-29 13:08:58 -0800

    ASoC: msm: t86519a1 wm8998 integration
    * Squash of the following:
    
    ASoC: msm: cleanup t86519a1 wm8998 integration
    
    - Fix an unmatched clk_put
    - Remove wingtech way of enabling QUAT clock.  It's been integrated
      upstream
    
    ASoC: msm8x16: wm8998 is renamed to vegas
    
    ASoC: msm8x16: wm8998 is renamed vegas part 2...
    
    ASoC: msm8x16-wcd: enable the micbias for sub mic loopback test
    
    Add audio route for sub mic
    
    Change-Id: I6ced934c234dafb487ee3a54814d6d8947e44fcb

--- sound/soc/codecs/msm8x16-wcd.c
+++ sound/soc/codecs/msm8x16-wcd.c
@@ -4323,10 +4323,14 @@ static const struct snd_soc_dapm_route audio_map[] = {
 	{"MIC BIAS External2", NULL, "INT_LDO_H"},
 	{"MIC BIAS Internal1", NULL, "MICBIAS_REGULATOR"},
 	{"MIC BIAS Internal2", NULL, "MICBIAS_REGULATOR"},
+#if defined(CONFIG_MACH_LETTUCE)
 	{"MIC BIAS Internal3", NULL, "MICBIAS_REGULATOR"}, //legen
+#endif
 	{"MIC BIAS External", NULL, "MICBIAS_REGULATOR"},
 	{"MIC BIAS External2", NULL, "MICBIAS_REGULATOR"},
+#if defined(CONFIG_MACH_LETTUCE)
 	{"MIC BIAS External3", NULL, "MICBIAS_REGULATOR"}, //legen
+#endif
 };
 
 static int msm8x16_wcd_startup(struct snd_pcm_substream *substream,
